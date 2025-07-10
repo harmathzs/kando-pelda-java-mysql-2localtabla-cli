315c3483d7d97a692062311f79f2e1de7e73f295
Zsolt Harmath <harmathzs@gmail.com>
2025-07-10 10:56:00 +0200

    Return all database records as a big map

--- src/MysqlService.java
+++ src/MysqlService.java
@@ -1,7 +1,15 @@
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MysqlService {
-    public static void getDogsAndOwners(String dbname, String username, String pass) {
+    public static Map<String, List<Map<String, Object>>> getDogsAndOwners(String dbname, String username, String pass) {
+        Map<String, List<Map<String, Object>>> results = new HashMap<>();
+        results.put("dogs", new ArrayList<>());
+        results.put("owners", new ArrayList<>());
+
         String url = "jdbc:mysql://localhost:3306/"+dbname;
 
         String query = """
@@ -49,11 +57,31 @@ public class MysqlService {
 
             // Process the result set
             while (rs.next()) {
+                Map<String, Object> dogMap = new HashMap<>();
+
                 int dog_id = rs.getInt("dog_id");
+                dogMap.put("dog_id", dog_id);
                 String dog_name = rs.getString("dog_name");
+                dogMap.put("dog_name", dog_name);
+                float age = rs.getFloat("age");
+                dogMap.put("age", age);
+                boolean male = rs.getBoolean("male");
+                dogMap.put("male", male);
+                int ownerid = rs.getInt("ownerid");
+                dogMap.put("ownerid", ownerid);
+
+                if (dog_name!=null) results.get("dogs").add(dogMap);
+
+                Map<String, Object> ownerMap = new HashMap<>();
+
                 int owner_id = rs.getInt("owner_id");
+                ownerMap.put("owner_id", owner_id);
                 String owner_name = rs.getString("owner_name");
-                System.out.println("dog_id: " + dog_id + ", dog_name: " + dog_name + ", owner_id: " + owner_id + ", owner_name: " + owner_name);
+                ownerMap.put("owner_name", owner_name);
+
+                if (owner_name!=null) results.get("owners").add(ownerMap);
+
+                //System.out.println("dog_id: " + dog_id + ", dog_name: " + dog_name + ", owner_id: " + owner_id + ", owner_name: " + owner_name);
             }
 
             // Close resources
@@ -63,6 +91,8 @@ public class MysqlService {
         } catch (SQLException | ClassNotFoundException e) {
             e.printStackTrace();
         }
+
+        return results;
     }
     public static void upsertDogs() {
 
--- src/MysqlTest.java
+++ src/MysqlTest.java
@@ -1,9 +1,25 @@
+import com.mysql.cj.xdevapi.JsonParser;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
 
 public class MysqlTest {
     @Test
     public void testgetDogsAndOwners() {
-        MysqlService.getDogsAndOwners("dogs_and_owners", "root", "");
+        Map<String, List<Map<String, Object>>> results = MysqlService.getDogsAndOwners("dogs_and_owners", "root", "");
+        System.out.println(results);
+        // {dogs=[
+            // {dog_name=Fido, ownerid=1, age=3.0, male=true, dog_id=1},
+            // {dog_name=Fifi, ownerid=2, age=1.0, male=false, dog_id=2},
+            // {dog_name=Oscar, ownerid=1, age=4.0, male=true, dog_id=3}],
+        // owners=[
+            // {owner_name=John Smith, owner_id=1},
+            // {owner_name=Jane Doe, owner_id=2},
+            // {owner_name=John Smith, owner_id=1},
+            // {owner_name=Mia Gordon, owner_id=3}]}
+
+        // TODO - Owners can appear duplicated! See: John Smith
     }
 
     @Test
