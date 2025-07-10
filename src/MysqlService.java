2007749796dabcf13e79cad64b3c997862b2bd5c
Zsolt Harmath <harmathzs@gmail.com>
2025-07-10 09:47:04 +0200

    Add MysqlService and Junit test boilerplate

---
+++ src/MysqlService.java
@@ -0,0 +1,17 @@
+public class MysqlService {
+    public static void getDogsAndOwners() {
+
+    }
+    public static void upsertDogs() {
+
+    }
+    public static void upsertOwners() {
+
+    }
+    public static void deleteDogs() {
+
+    }
+    public static void deleteOwners() {
+
+    }
+}
---
+++ src/MysqlTest.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.Test;
+
+public class MysqlTest {
+    @Test
+    public void testgetDogsAndOwners() {
+        MysqlService.getDogsAndOwners();
+    }
+
+    @Test
+    public void testupsertDogs() {
+        MysqlService.upsertDogs();
+    }
+
+    @Test
+    public void testupsertOwners() {
+        MysqlService.upsertOwners();
+    }
+
+    @Test
+    public void testdeleteDogs() {
+        MysqlService.deleteDogs();
+    }
+
+    @Test
+    public void testdeleteOwners() {
+        MysqlService.deleteOwners();
+    }
+}
