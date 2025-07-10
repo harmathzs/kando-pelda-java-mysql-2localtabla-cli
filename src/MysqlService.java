d10d3c4b576dc7f810350e767b5f40523d5ffb9f
Zsolt Harmath <harmathzs@gmail.com>
2025-07-10 10:37:21 +0200

    Add SELECT UNION query for dogs and owners

--- src/MysqlService.java
+++ src/MysqlService.java
@@ -4,7 +4,37 @@ public class MysqlService {
     public static void getDogsAndOwners(String dbname, String username, String pass) {
         String url = "jdbc:mysql://localhost:3306/"+dbname;
 
-        String query = "SELECT id, name FROM owners";
+        String query = """
+            SELECT
+              dogs.id AS dog_id,
+              dogs.name AS dog_name,
+              dogs.age,
+              dogs.male,
+              dogs.ownerid,
+              owners.id AS owner_id,
+              owners.name AS owner_name
+            FROM
+              dogs
+            LEFT JOIN
+              owners ON dogs.ownerid = owners.id
+            
+            UNION
+            
+            SELECT
+              dogs.id AS dog_id,
+              dogs.name AS dog_name,
+              dogs.age,
+              dogs.male,
+              dogs.ownerid,
+              owners.id AS owner_id,
+              owners.name AS owner_name
+            FROM
+              owners
+            LEFT JOIN
+              dogs ON dogs.ownerid = owners.id
+            """;
+        // WHERE dogs.id IS NULL;
+
 
         try {
             // Load MySQL JDBC Driver
@@ -19,9 +49,11 @@ public class MysqlService {
 
             // Process the result set
             while (rs.next()) {
-                int id = rs.getInt("id");
-                String name = rs.getString("name");
-                System.out.println("ID: " + id + ", Name: " + name);
+                int dog_id = rs.getInt("dog_id");
+                String dog_name = rs.getString("dog_name");
+                int owner_id = rs.getInt("owner_id");
+                String owner_name = rs.getString("owner_name");
+                System.out.println("dog_id: " + dog_id + ", dog_name: " + dog_name + ", owner_id: " + owner_id + ", owner_name: " + owner_name);
             }
 
             // Close resources
