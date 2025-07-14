1281bdd7205360aa5d21c1489d33588123f90829
Zsolt Harmath <harmathzs@gmail.com>
2025-07-14 09:25:20 +0200

    Prepare Main for showing data

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.Collections;
+import java.util.List;
 
+public class Main {
+    public static void showData() {
+        // dogs
+        List<Dog> dogs = MysqlService.queryDogs("localhost", "dogs_and_owners", "root", "", Collections.emptySet());
+        System.out.println("Dogs:");
+        System.out.println("------");
+        for (Dog dog: dogs) {
+            System.out.println("id: "+dog.getId());
+        }
+        System.out.println("------");
+        // dog_owners
+        List<Owner> owners = MysqlService.queryOwners("localhost", "dogs_and_owners", "root", "", Collections.emptySet());
+        System.out.println("Owners:");
+        System.out.println("------");
+        for (Owner owner: owners) {
+            System.out.println("id: "+owner.getId());
+        }
+        System.out.println("------");
+    }
+
+    public static void main(String[] args) {
+        showData();
     }
 }
