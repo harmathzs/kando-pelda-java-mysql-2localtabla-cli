554d41c6513bed1ee35d50da86432772317db434
Zsolt Harmath <harmathzs@gmail.com>
2025-07-14 11:55:23 +0200

    Add isRunningTest

--- src/MainTest.java
+++ src/MainTest.java
@@ -1,6 +1,17 @@
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class MainTest {
+    @BeforeAll
+    public static void testBeforeAll() {
+        Main.isRunningTest = true;
+    }
+    @AfterAll
+    public static void testAfterAll() {
+        Main.isRunningTest = false;
+    }
+
     @Test
     public void testMain() {
         Main.main(null);
